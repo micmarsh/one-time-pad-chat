c7b2ed0dc826c42b1a339b2d2cb009faf85f1b8c
micmarsh <michael.marsh42@gmail.com>
2014-03-15 16:25:56 -0400

    starting to move things over to chat namespace

---
+++ src/otpclj/chat.clj
@@ -0,0 +1,22 @@
+(ns otpclj.chat
+    (:use [otpclj.encrypt :only [otp]]
+          [otpclj.padding :only [add-padding remove-padding]]))
+
+(defn main-loop [keys paddings]
+    (if (empty? keys)
+        (println "you're all out of encryption keys!")
+        (let [current-padding (first paddings)
+              pad (partial add-padding current-padding)
+              unpad (partial remove-padding current-padding)
+
+              encrypt-base (partial otp (first keys))
+              encrypt (comp encrypt-base pad)
+              decrypt (comp unpad encrypt-base)
+
+              message (read-line)
+              encrypted (-> message pad encrypt)
+              decrypted (-> encrypted decrypt unpad)]
+            (println "your message:" message)
+            (println "encrypted:" encrypted)
+            (println "decrypted:" decrypted)
+            (recur (rest keys) (rest paddings)))))
--- src/otpclj/core.clj
+++ src/otpclj/core.clj
@@ -1,28 +1,8 @@
 (ns otpclj.core
     (:use [otpclj.constants :only [KEYS PADDINGS]]
-          [otpclj.encrypt :only [otp]]
-          [otpclj.padding :only [add-padding remove-padding]]
+          [otpclj.chat :only [main-loop]]
           [otpclj.generate :only [generate-constants]]
           [clojure.tools.cli :only [parse-opts]]))
-
-(defn main-loop [keys paddings]
-    (if (empty? keys)
-        (println "you're all out of encryption keys!")
-        (let [current-padding (first paddings)
-              pad (partial add-padding current-padding)
-              unpad (partial remove-padding current-padding)
-
-              encrypt-base (partial otp (first keys))
-              encrypt (comp encrypt-base pad)
-              decrypt (comp unpad encrypt-base)
-
-              message (read-line)
-              encrypted (-> message pad encrypt)
-              decrypted (-> encrypted decrypt unpad)]
-            (println "your message:" message)
-            (println "encrypted:" encrypted)
-            (println "decrypted:" decrypted)
-            (recur (rest keys) (rest paddings)))))
 
 (def parse-int #(Integer/parseInt %))
 
