03bfbfc5aa572436e9cbe99d6946aceaa9a9a188
micmarsh <michael.marsh42@gmail.com>
2014-03-15 21:37:30 -0400

    attempting core.async again

--- src/otpclj/chat.clj
+++ src/otpclj/chat.clj
@@ -1,7 +1,7 @@
 (ns otpclj.chat
     (:use [otpclj.encrypt :only [otp]]
           [otpclj.padding :only [add-padding remove-padding]]
-          [otpclj.connection :only [make-messenger]]
+          [otpclj.connection :only [incoming send-message]]
           [clojure.core.async :only [go-loop <!]]))
 
 (defn crypto-fns [current-key current-padding]
@@ -12,20 +12,23 @@
           decrypt (comp unpad encrypt-base)]
         {:encrypt encrypt :decrypt decrypt}))
 
-(def send-message (make-messenger println "asss"))
-
 (defn main-loop [KEYS PADDINGS]
+
+    (go-loop [keys KEYS paddings PADDINGS]
+        (let [message (<! incoming)
+              {:keys [decrypt]}
+                  (crypto-fns (first keys) (first paddings))]
+            (->> message decrypt (println "they say:")))
+        (recur (rest keys) (rest paddings)))
+
     (println "starting main loop!")
         (loop [keys KEYS paddings PADDINGS] 
             (if (empty? keys)
                 (println "you're all out of encryption keys!")
-                (let [{:keys [encrypt decrypt]} 
-                            (crypto-fns (first keys) (first paddings))
+                (let [{:keys [encrypt]} 
+                            (crypto-fns (first keys) (first paddings))]
                       message (read-line)
                       encrypted (encrypt message)
-                      decrypted (decrypt encrypted)]
-                    (println "your message:" message)
-                    (println "encrypted:" encrypted)
-                    (println "decrypted:" decrypted)
+                    (println "you say:" message)
                     (send-message encrypted)
                     (recur (rest keys) (rest paddings))))))
