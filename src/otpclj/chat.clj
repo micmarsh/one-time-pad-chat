1fc41a872f9e226c39dbee9db3d9c5f2fa05b270
micmarsh <michael.marsh42@gmail.com>
2014-03-16 13:08:07 -0400

    make sockets interface cleaner, seem to have lost decryption

--- src/otpclj/chat.clj
+++ src/otpclj/chat.clj
@@ -1,22 +1,29 @@
 (ns otpclj.chat
     (:use [otpclj.crypto :only [make-encryptor make-decryptor]]
-          [otpclj.connection :only [incoming send-message]]
-          [clojure.core.async :only [go-loop <!]]))
+          [otpclj.connection :only [connect!]]
+          [clojure.core.async :only [go-loop <! chan put!]]))
+
+(defn show-received [message]
+    (println "they say:" message))
+
+(def CHAT_SERVER "ws://chitty-chatty.herokuapp.com/chat/asss")
 
 (defn main-loop [KEYS PADDINGS]
+    (let [outgoing (chan)
+          incoming (connect! CHAT_SERVER outgoing)]
 
-    (go-loop [keys KEYS paddings PADDINGS]
-        (let [message (<! incoming)
-              decrypt (make-decryptor (first keys) (first paddings))]
-            (->> message decrypt (println "they say:")))
-        (recur (rest keys) (rest paddings)))
+        (go-loop [keys KEYS paddings PADDINGS]
+            (let [message (<! incoming)
+                  decrypt (make-decryptor (first keys) (first paddings))]
+                (-> message decrypt show-received))
+            (recur (rest keys) (rest paddings)))
 
-    (println "starting main loop!")
-        (loop [keys KEYS paddings PADDINGS] 
-            (if (empty? keys)
-                (println "you're all out of encryption keys!")
-                (let [encrypt (make-encryptor (first keys) (first paddings))
-                      message (read-line)
-                      encrypted (encrypt message)]
-                    (send-message encrypted)
-                    (recur (rest keys) (rest paddings))))))
+        (println "starting main loop!")
+            (loop [keys KEYS paddings PADDINGS] 
+                (if (empty? keys)
+                    (println "you're all out of encryption keys!")
+                    (let [encrypt (make-encryptor (first keys) (first paddings))
+                          message (read-line)
+                          encrypted (encrypt message)]
+                        (put! outgoing message)
+                        (recur (rest keys) (rest paddings)))))))
--- src/otpclj/connection.clj
+++ src/otpclj/connection.clj
@@ -1,30 +1,31 @@
 (ns otpclj.connection
-    (:use [clojure.core.async :only [chan put!]]))
+    (:use [clojure.core.async :only [chan put! go >! <!]]))
 (import [org.java_websocket.client WebSocketClient])
 
-(def CHAT_SERVER "ws://chitty-chatty.herokuapp.com/chat/asss")
+; 
 
-(def incoming (chan))
+(defn- make-sender [where incoming]
+    (let [last-messsage (atom nil)
+          ^WebSocketClient socket (proxy [WebSocketClient] 
+            [(java.net.URI. where)]
+            (onOpen [data] (println "opened socket!"))
+            (onClose [code reason remote] (println "closed socket because" reason))
+            (onMessage [message]
+                (when 
+                    (not (= message @last-messsage)) 
+                        (put! incoming message)))
+            (onError [exception] (println "hey socket error")))]
+          (.connect socket)
+        (fn [message]
+            (reset! last-messsage message)
+            (.send socket message))))
 
-(def last-messsage (atom nil))
-
-(def socket
-    (proxy [WebSocketClient] 
-        [(java.net.URI. CHAT_SERVER)]
-        (onOpen [data] (println "opened socket!"))
-        (onClose [code reason remote] (println "closed socket because" reason))
-        (onMessage [message]
-            (when 
-                (not (= message @last-messsage)) 
-                    (put! incoming message)))
-        (onError [exception] (println "hey socket error"))))
-
-(.connect socket)
-
-(defn send! [^WebSocketClient client message]
-    (reset! last-messsage message)
-    (.send client message))
-
-(def send-message (partial send! socket))
+(defn connect! [where outgoing]
+    (let [incoming (chan)
+          send! (make-sender where incoming)]
+        (go (while true
+            (let [message (<! outgoing)]
+                (send! message))))
+        incoming))
 
 
