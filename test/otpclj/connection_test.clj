963344fbf7395eca3ac9f36d715089d69d28d5ba
micmarsh <michael.marsh42@gmail.com>
2014-03-16 13:53:20 -0400

    added connection test

---
+++ test/otpclj/connection_test.clj
@@ -0,0 +1,18 @@
+(ns otpclj.connection-test
+  (:use midje.sweet
+        [clojure.core.async :only [chan put! take! <!!]])               
+  (:require [otpclj.connection :as c]))
+
+(def ECHO_URL "ws://echo.websocket.org")
+
+(def outgoing (chan))
+
+(def echo (c/connect! ECHO_URL outgoing))
+
+(put! outgoing "hey")
+(put! outgoing "what's up")
+
+(fact "reads things back from echo"
+    (<!! echo) => "hey"
+    (<!! echo) => "what's up")
+    
